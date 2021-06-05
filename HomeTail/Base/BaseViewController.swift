bcf6559635a5efd40dfe216044840cdff136934a
taeuk <nous178@naver.com>
2021-06-06 00:18:06 +0900

    feat: BaseViewController, 스토리보드 없이 코드연결

---
+++ HomeTail/Base/BaseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  HomeTail
+//
+//  Created by taeuk on 2021/06/04.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        configuration()
+        constraints()
+        
+    }
+    
+    // 설정
+    @objc dynamic func configuration() {
+        
+    }
+    
+    // 제약
+    @objc dynamic func constraints() {
+        
+    }
+}
+
---
+++ HomeTail/View/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  HomeTail
+//
+//  Created by taeuk on 2021/06/06.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension HomeViewController {
+    
+    override func configuration() {
+        
+    }
+    
+    override func constraints() {
+        
+    }
+}
