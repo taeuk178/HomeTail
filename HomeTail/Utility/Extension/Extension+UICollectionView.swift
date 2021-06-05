1f07c05550aeeebf23502a77dd776027d623b4a9
taeuk <nous178@naver.com>
2021-06-06 02:48:59 +0900

    feat: Extension+UICollectionView

---
+++ HomeTail/Utility/Extension/Extension+UICollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  Extension+UICollectionView.swift
+//  HomeTail
+//
+//  Created by taeuk on 2021/06/06.
+//
+
+import UIKit
+
+extension UICollectionView {
+    
+    func registerCell<T: UICollectionViewCell>(_: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueTableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell: T = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else { fatalError("Generic UITableViewCell is Error") }
+        
+        return cell
+    }
+}
+
+extension UICollectionViewCell: ReusableCell {}
--- HomeTail/Utility/Extension/Extension+UITableView.swift
+++ HomeTail/Utility/Extension/Extension+UITableView.swift
@@ -21,10 +21,3 @@ extension UITableView {
 }
 
 extension UITableViewCell: ReusableCell {}
-
-protocol ReusableCell {
-    static var reuseIdentifier: String { get }
-}
-extension ReusableCell {
-    static var reuseIdentifier: String { return String(describing: self) }
-}
